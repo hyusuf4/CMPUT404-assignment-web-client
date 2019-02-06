e66b3ebf29a6fce906e3caa044b9dee493326663
Hamdi Yusuf <hyusuf63@gmail.com>
2019-02-05 23:05:15 -0700

    finished

--- httpclient.py
+++ httpclient.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 # coding: utf-8
 # Copyright 2016 Abram Hindle, https://github.com/tywtyw2002, and https://github.com/treedust
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -33,25 +33,31 @@ class HTTPResponse(object):
         self.body = body
 
 class HTTPClient(object):
-    #def get_host_port(self,url):
-
+    def get_host_port(self,url):
+        url_object=urllib.parse.urlparse(url)
+        host_name=url_object.hostname
+        if url_object.port==None:
+            port=80
+        else:
+            port=url_object.port
+        return (host_name,port)
     def connect(self, host, port):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect((host, port))
-        return None
+        return self.socket
 
     def get_code(self, data):
-        return None
+        return int(data.split()[1])
 
     def get_headers(self,data):
-        return None
+        return data.split('\r\n\r\n')[0]
 
     def get_body(self, data):
-        return None
-    
+        return data.split('\r\n\r\n')[1]
+
     def sendall(self, data):
         self.socket.sendall(data.encode('utf-8'))
-        
+
     def close(self):
         self.socket.close()
 
@@ -65,16 +71,50 @@ class HTTPClient(object):
                 buffer.extend(part)
             else:
                 done = not part
-        return buffer.decode('utf-8')
+        return buffer.decode('ISO-8859-1')
 
     def GET(self, url, args=None):
-        code = 500
-        body = ""
+        (host,port)=self.get_host_port(url)
+        self.connect(host,port)
+        url_object=urllib.parse.urlparse(url)
+        if(not url_object.path):
+            path='/'
+        else:
+            path=url_object.path
+
+        get_request='GET {} HTTP/1.1\r\n'.format(path)
+        get_request+='Host: {}\r\n'.format(url_object.hostname)
+        get_request+='Content-type: application/x-www-form-urlencoded\r\n'
+        get_request+='Connection: close\r\n\r\n'
+        self.sendall(get_request)
+        data=self.recvall(self.socket)
+        code=self.get_code(str(data))
+        body = self.get_body(str(data))
+        self.close()
         return HTTPResponse(code, body)
 
     def POST(self, url, args=None):
-        code = 500
-        body = ""
+        (host,port)=self.get_host_port(url)
+        self.connect(host,port)
+        url_object=urllib.parse.urlparse(url)
+        if(not url_object.path):
+            path='/'
+        else:
+            path=url_object.path
+        if args== None:
+            post=''
+        else:
+            post=urllib.parse.urlencode(args)
+        post_request='POST {} HTTP/1.1\r\n'.format(path)
+        post_request+='Host: {}\r\n'.format(url_object.hostname)
+        post_request+='Content-type: application/x-www-form-urlencoded\r\n'
+        post_request+='Content-Length: {}\r\n'.format(len(post))
+        post_request+='Connection: close\r\n\r\n'
+        post_request+=post
+        self.sendall(post_request)
+        data=self.recvall(self.socket)
+        code=self.get_code(str(data))
+        body = self.get_body(str(data))
         return HTTPResponse(code, body)
 
     def command(self, url, command="GET", args=None):
@@ -82,7 +122,7 @@ class HTTPClient(object):
             return self.POST( url, args )
         else:
             return self.GET( url, args )
-    
+
 if __name__ == "__main__":
     client = HTTPClient()
     command = "GET"
